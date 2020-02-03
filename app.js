472eda11b4b8c0017bc7cdaafd944ba169c65aab
Petzi <daboarderpjb@gmail.com>
2020-02-03 18:30:49 +0100

    server

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http')
 
  const server = http.createServer((req, res) =>{
-    console.log(req)
+    //console.log(req.url, req.method, req.headers);
+
+     const url = req.url;
+     if(url ==='/'){
+         return
+     }
+
+    res.setHeader('Content-Type', 'text/html')
+     res.write('<html>')
+     res.write('<head></head>')
+     res.write('<body><h1>Hello</h1></body>')
+     res.write('</html>')
+     res.end();
+
  });
 
 server.listen(3000);
