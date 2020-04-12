4ccfca5892d1355486214a3fb43c5fd9b7da17b1
Petzi <daboarderpjb@gmail.com>
2020-04-12 21:49:12 +0200

    input

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const http = require('http')
+const fs = require('fs')
+const port = 8000;
 
  const server = http.createServer((req, res) =>{
-    //console.log(req.url, req.method, req.headers);
-
-     const url = req.url;
-     if(url ==='/'){
-         return
-     }
-
-    res.setHeader('Content-Type', 'text/html')
-     res.write('<html>')
-     res.write('<head></head>')
-     res.write('<body><h1>Hello</h1></body>')
-     res.write('</html>')
-     res.end();
-
+    const url = req.url;
+    const method = req.method;
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html><body><center><h1>Hallo Petzi</h1></center>');
+        res.write('<form action="/message" method="POST"><input type="text" name="Nachricht"><button type="submit">Send</button></form>');
+        res.write('</body></html>');
+        return res.end();
+    }
+    if (url ==='/message' && method ==='POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        });
+        req.on ('end', () =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const nachricht = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', nachricht);
+        });
+        res.statusCode=302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
  });
 
-server.listen(3000);
+server.listen(port);
 
 
